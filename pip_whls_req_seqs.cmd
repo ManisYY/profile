d6a9eaa4e61c84890857219425bc0ce2fa4b1d1d
ManisY <erqing091092@hotmail.com>
2017-06-28 15:18:25 +0800

    Create pip_whls_req_seqs.cmd
    记录离线安装的依赖顺序，一个个地去试验是在是太痛苦了。
    
    To record the whl dependency sequence in offline installation, because test them one by one is too painful.

---
+++ pip_whls_req_seqs.cmd
@@ -0,0 +1,96 @@
+pip install D:\toolkit\python_extend_tools\jsonschema-2.6.0-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\mistune-0.7.4-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\decorator-4.0.11-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\MarkupSafe-1.0-cp36-cp36m-win32.whl
+==========================================================================
+pip install D:\toolkit\python_extend_tools\Jinja2-2.9.6-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\colorama-0.3.7-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\entrypoints-0.2.2-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\six-1.10.0-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\webencodings-0.5.1-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\html5lib-0.999999999-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\bleach-2.0.0-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\cycler-0.10.0-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\pytz-2017.2-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\pyparsing-2.2.0-py2.py3-none-any.whl
+==========================================================================
+pip install D:\toolkit\python_extend_tools\numpy-1.12.1-cp36-none-win32.whl
+--pip install D:\toolkit\python_extend_tools\numpy-1.12.1-cp36-none-win_amd64.whl
+==========================================================================
+pip install D:\toolkit\python_extend_tools\python_dateutil-2.6.0-py2.py3-none-any.whl
+==========================================================================
+pip install D:\toolkit\python_extend_tools\matplotlib-2.0.0-cp36-cp36m-win32.whl
+==========================================================================
+pip install D:\toolkit\python_extend_tools\ipython_genutils-0.2.0-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\traitlets-4.3.1-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\pyzmq-16.0.2-cp36-cp36m-win32.whl
+pip install D:\toolkit\python_extend_tools\pickleshare-0.7.4-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\wcwidth-0.1.7-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\prompt_toolkit-1.0.14-py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\Pygments-2.2.0-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\simplegeneric-0.8.1-py2.py3-none-any.whl
+==========================================================================
+
+pip install D:\toolkit\python_extend_tools\ipython-5.3.0-py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\jupyter_core-4.3.0-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\nbformat-4.3.0-py2.py3-none-any.whl
+==========================================================================
+
+pip install D:\toolkit\python_extend_tools\jupyter_client-5.0.1-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\tornado-4.5.1-cp36-cp36m-win32.whl
+pip install D:\toolkit\python_extend_tools\ipykernel-4.5.2-py2.py3-none-any.whl
+==========================================================================
+pip install D:\toolkit\python_extend_tools\jupyter_console-5.1.0-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\testpath-0.3-py2.py3-none-any.whl
+
+==========================================================================
+pip install D:\toolkit\python_extend_tools\pandocfilters-1.4.1-py2.py3-none-any.whl.whl
+pip install D:\toolkit\python_extend_tools\nbconvert-5.1.1-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\notebook-5.0.0-py2.py3-none-any.whl
+
+pip install D:\toolkit\python_extend_tools\widgetsnbextension-3.0.0a6-py2.py3-none-any.whl
+
+pip install D:\toolkit\python_extend_tools\ipywidgets-7.0.0a3-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\qtconsole-4.3.0-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\jupyter-1.0.0-py2.py3-none-any.whl
+===========================================================================
+
+pip install D:\toolkit\python_extend_tools\pandas-0.19.2-cp36-cp36m-win32.whl
+===========================================================================
+
+=======================install to spyder======================================
+pip install D:\toolkit\python_extend_tools\rope_py3k-0.9.4.post1-py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\chardet-3.0.4-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\numpydoc-0.6.0-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\pyflakes-1.5.0-py2.py3-none-any.whl
+
+pip install D:\toolkit\python_extend_tools\sphinxcontrib_websupport-1.0.1-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\alabaster-0.7.10-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\docutils-0.13.1-py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\snowballstemmer-1.2.1-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\imagesize-0.7.1-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\Babel-2.4.0-py2.py3-none-any.whl
+
+pip install D:\toolkit\python_extend_tools\requests-2.13.0-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\Sphinx-1.6.2-py2.py3-none-any.whl
+
+pip install D:\toolkit\python_extend_tools\QtPy-1.2.1-py2.py3-none-any.whl
+
+pip install D:\toolkit\python_extend_tools\QtAwesome-0.4.4-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\jedi-0.10.2-py2.py3-none-any.whl
+
+pip install D:\toolkit\python_extend_tools\isort-4.2.15-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\mccabe-0.6.1-py2.py3-none-any.whl
+
+pip install D:\toolkit\python_extend_tools\lazy_object_proxy-1.3.1-cp36-cp36m-win32.whl
+pip install D:\toolkit\python_extend_tools\wrapt-1.10.10-cp36-cp36m-win32.whl
+
+pip install D:\toolkit\python_extend_tools\astroid-1.5.2-py2.py3-none-any.whl
+pip install D:\toolkit\python_extend_tools\pylint-1.7.1-py2.py3-none-any.whl
+
+pip install D:\toolkit\python_extend_tools\pep8-1.7.0-py2.py3-none-any.whl
+
+pip install D:\toolkit\python_extend_tools\spyder-3.1.4-py3-none-any.whl
+
+
+
